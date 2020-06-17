02f86a24ed24273be9fa2504d37b3432bd9fa9ac
Marie Verdonck <maria.verdonck@atmire.com>
2020-06-17 15:13:21 +0200

    authorization token added for /config/properties endpoint tests

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ConfigurationRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ConfigurationRestRepositoryIT.java
@@ -21,7 +21,8 @@ import org.junit.Test;
 public class ConfigurationRestRepositoryIT extends AbstractControllerIntegrationTest {
     @Test
     public void getSingleValue() throws Exception {
-        getClient().perform(get("/api/config/properties/configuration.exposed.single.value"))
+        String adminToken = getAuthToken(admin.getEmail(), password);
+        getClient(adminToken).perform(get("/api/config/properties/configuration.exposed.single.value"))
             .andExpect(jsonPath("$.values[0]", is("public_value")))
             .andExpect(jsonPath("$.type", is("property")))
             .andExpect(jsonPath("$.name", is("configuration.exposed.single.value")))
@@ -30,20 +31,23 @@ public class ConfigurationRestRepositoryIT extends AbstractControllerIntegration
 
     @Test
     public void getArrayValue() throws Exception {
-        getClient().perform(get("/api/config/properties/configuration.exposed.array.value"))
+        String adminToken = getAuthToken(admin.getEmail(), password);
+        getClient(adminToken).perform(get("/api/config/properties/configuration.exposed.array.value"))
         .andExpect(jsonPath("$.values[0]", is("public_value_1")))
         .andExpect(jsonPath("$.values[1]", is("public_value_2")));
     }
 
     @Test
     public void getNonExistingValue() throws Exception {
-        getClient().perform(get("/api/config/properties/configuration.not.existing"))
+        String adminToken = getAuthToken(admin.getEmail(), password);
+        getClient(adminToken).perform(get("/api/config/properties/configuration.not.existing"))
             .andExpect(status().isNotFound());
     }
 
     @Test
     public void getNonExposedValue() throws Exception {
-        getClient().perform(get("/api/config/properties/configuration.not.exposed"))
+        String adminToken = getAuthToken(admin.getEmail(), password);
+        getClient(adminToken).perform(get("/api/config/properties/configuration.not.exposed"))
             .andExpect(status().isNotFound());
     }
 
