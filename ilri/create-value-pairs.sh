a24fc23e9da4129c1aa3f95539f9ddfdaa0d2207
Alan Orth <alan.orth@gmail.com>
2021-09-24 13:59:53 +0300

    ilri/create-value-pairs.sh: Add more space

--- ilri/create-value-pairs.sh
+++ ilri/create-value-pairs.sh
@@ -6,10 +6,10 @@ printf '<value-pairs value-pairs-name="%s">\n' $2
 
 while read -r line
 do
-  printf '<pair>\n'
-  printf '  <displayed-value>%s</displayed-value>\n' "$line"
-  printf '  <stored-value>%s</stored-value>\n' "$line"
-  printf '</pair>\n'
+  printf '  <pair>\n'
+  printf '    <displayed-value>%s</displayed-value>\n' "$line"
+  printf '    <stored-value>%s</stored-value>\n' "$line"
+  printf '  </pair>\n'
 done < $1
 
 printf '</value-pairs>'
