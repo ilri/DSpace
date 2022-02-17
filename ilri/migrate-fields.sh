9345e8d9ad7913b0f99bf8a6291d3dbdc7f0f040
Alan Orth <alan.orth@gmail.com>
2022-02-17 11:10:17 +0300

    ilri/migrate-fields.sh: use field names
    This is easier to manage when you're migrating many fields and have
    to deal with a development and production instance where the IDs
    might be different.

--- ilri/migrate-fields.sh
+++ ilri/migrate-fields.sh
@@ -9,41 +9,61 @@
 # Exit on first error
 set -o errexit
 
-# IDs of fields to move, in this format:
+# Names of fields to move, in this format:
 #
 # old_field new_field
 #
 # fields are separated with tabs or spaces. Uses bash's `mapfile` to read into
 # an array.
 mapfile -t fields_to_move <<TO_MOVE
-240 247 #cg.creator.id→cg.creator.identifier
-34  158 #dc.format.extent→dcterms.extent
-15  170 #dc.date.issued→dcterms.issued
-27  138 #dc.description.abstract→dcterms.abstract
-26  156 #dc.description→dcterms.description
-29  248 #dc.description.sponsorship→cg.contributor.donor
-68  249 #dc.description.version→cg.reviewStatus
-214 250 #cg.fulltextstatus→cg.howPublished
-18  146 #dc.identifier.citation→dcterms.bibliographicCitation
-206 139 #cg.identifier.status→dcterms.accessRights
-38  172 #dc.language.iso→dcterms.language
-113 180 #cg.link.reference→dcterms.relation
-39  178 #dc.publisher→dcterms.publisher
-43  166 #dc.relation.ispartofseries→dcterms.isPartOf
-53  173 #dc.rights→dcterms.license
-55  251 #dc.source→cg.journal
-57  187 #dc.subject→dcterms.subject
-109 191 #dc.type→dcterms.type
-20  252 #dc.identifier.isbn→cg.isbn
-21  253 #dc.identifier.issn→cg.issn
-213  144 #cg.targetaudience→dcterms.audience
+dc.format.extent            dcterms.extent
+dc.date.issued              dcterms.issued
+dc.description.abstract     dcterms.abstract
+dc.description              dcterms.description
+dc.description.sponsorship  cg.contributor.donor
+dc.description.version      cg.reviewStatus
+dc.identifier.citation      dcterms.bibliographicCitation
+dc.language.iso             dcterms.language
+dc.publisher                dcterms.publisher
+dc.relation.ispartofseries  dcterms.isPartOf
+dc.rights                   dcterms.license
+dc.source                   cg.journal
+dc.subject                  dcterms.subject
+dc.type                     dcterms.type
+dc.identifier.isbn          cg.isbn
+dc.identifier.issn          cg.issn
 TO_MOVE
 
 # psql stuff
-readonly DATABASE_NAME=dspace63cgcorev2
+readonly DATABASE_NAME=dspacetest
 readonly PSQL_BIN="/usr/bin/env psql"
-# clean startup, and only print results
-readonly PSQL_OPTS="--no-psqlrc --tuples-only -U postgres -h localhost --dbname $DATABASE_NAME"
+# clean startup, and only print results without whitespace
+readonly PSQL_OPTS="--no-psqlrc --tuples-only -A -U postgres -h localhost --dbname $DATABASE_NAME"
+
+lookup_field_id() {
+    local field=$1
+    local schema=$(echo $field | cut -d. -f1)
+    local element=$(echo $field | cut -d. -f2)
+    local qualifier=$(echo $field | cut -d. -f3)
+
+    # Check if schema (ie "dc") is set and get its ID
+    if [[ $schema ]]; then
+        local psql_cmd="SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id='${schema}'"
+        local schema_id=$($PSQL_BIN $PSQL_OPTS --command "$psql_cmd")
+    fi
+
+    # Check if field has an element qualifier, ie dc.description.abstract
+    if [[ $qualifier ]]; then
+        psql_cmd="SELECT metadata_field_id FROM metadatafieldregistry WHERE metadata_schema_id=${schema_id} AND element='${element}' AND qualifier='${qualifier}'"
+        local field_id=$($PSQL_BIN $PSQL_OPTS --command "$psql_cmd")
+    else
+        # If there is no qualifier then we need to use "IS NULL"
+        psql_cmd="SELECT metadata_field_id FROM metadatafieldregistry WHERE metadata_schema_id=${schema_id} AND element='${element}' AND qualifier IS NULL"
+        local field_id=$($PSQL_BIN $PSQL_OPTS --command "$psql_cmd")
+    fi
+
+    echo $field_id
+}
 
 migrate_field() {
     local old_id=$1
@@ -66,12 +86,25 @@ main() {
             continue
         fi
 
-        # call migrate_field() with format:
-        # migrate_field 66 109
-        migrate_field $row
+        field_from=$(echo $row | awk '{print $1}')
+        field_to=$(echo $row | awk '{print $2}')
 
-        # relax!
-        sleep 1
+        field_from_id=$(lookup_field_id $field_from)
+        if [[ ! $field_from_id ]]; then
+            echo "Make sure $field_from exists in the DSpace metadata registry."
+
+            exit 1
+        fi
+
+        field_to_id=$(lookup_field_id $field_to)
+        if [[ ! $field_to_id ]]; then
+            echo "Make sure $field_to exists in the DSpace metadata registry."
+
+            exit 1
+        fi
+
+        echo "Migrating $field_from → $field_to"
+        migrate_field $field_from_id $field_to_id
     done
 }
 
