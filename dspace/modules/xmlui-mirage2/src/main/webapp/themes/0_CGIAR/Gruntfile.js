fb2b4c45660872b8d77251c2dbf7b1d092d58bce
Alan Orth <alan.orth@gmail.com>
2022-05-13 11:46:39 +0300

    xmlui: update autoprefixer's browserslist config
    Autoprefixer started printing this error during build:
    
    > Replace Autoprefixer browsers option to Browserslist config.
    > Use browserslist key in package.json or .browserslistrc file.
    >
    > Using browsers option can cause errors. Browserslist config
    > can be used for Babel, Autoprefixer, postcss-normalize and other tools.

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/Gruntfile.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/Gruntfile.js
@@ -121,7 +121,7 @@ module.exports = function (grunt) {
                 options: {
                     processors: [
                         require('pixrem')(), // add fallbacks for rem units
-                        require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                        require('autoprefixer')(), // add vendor prefixes
                         require('cssnano')() // minify the result
                     ]
                 },
@@ -132,7 +132,7 @@ module.exports = function (grunt) {
                     map: true, // inline sourcemaps
                     processors: [
                         require('pixrem')(), // add fallbacks for rem units
-                        require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                        require('autoprefixer')(), // add vendor prefixes
                     ]
                 },
                 src: 'styles/*.css'
