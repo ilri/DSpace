bf489e0b209b4198d1488e3c592d6407506d3993
Alan Orth <alan.orth@gmail.com>
2022-12-07 09:54:14 +0100

    ilri/delete-metadata-values.py: bump version
    Clean header comments, sort imports, and bump version.

--- ilri/delete-metadata-values.py
+++ ilri/delete-metadata-values.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 #
-# delete-metadata-values.py 1.1.0
+# delete-metadata-values.py 1.2.0
 #
 # Copyright 2018–2020 Alan Orth.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-only
 #
 # ---
 #
@@ -30,16 +19,17 @@
 # that you can install with pip (I recommend setting up a Python virtual env
 # first):
 #
-#   $ pip install psycopg2-binary colorama
+#   $ pip install psycopg2 colorama
 #
 
 import argparse
-from colorama import Fore
 import csv
-import psycopg2
 import signal
 import sys
+
+import psycopg2
 import util
+from colorama import Fore
 
 
 def signal_handler(signal, frame):
