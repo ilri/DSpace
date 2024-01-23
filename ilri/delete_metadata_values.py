14dc005f874d828acedc1d737e2f3149ca19cffb
Alan Orth <alan.orth@gmail.com>
2024-01-23 12:47:39 +0300

    ilri: set connection read_only for dry run
    Python script interacting with the database can be read only when
    they are running in dry run mode.

--- ilri/delete_metadata_values.py
+++ ilri/delete_metadata_values.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# delete-metadata-values.py 1.2.4
+# delete-metadata-values.py 1.2.5
 #
 # Copyright Alan Orth.
 #
@@ -94,6 +94,9 @@ signal.signal(signal.SIGINT, signal_handler)
 conn = util.db_connect(
     args.database_name, args.database_user, args.database_pass, "localhost"
 )
+
+if args.dry_run:
+    conn.read_only = True
 
 cursor = conn.cursor()
 
