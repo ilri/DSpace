e75094de5dd2070daf82d3a25ddd491f41258b55
Alan Orth <alan.orth@gmail.com>
2025-11-03 10:35:49 +0300

    ilri: run black

--- ilri/generate_solr_statistics.py
+++ ilri/generate_solr_statistics.py
@@ -138,9 +138,9 @@ if atmire_cua:
     del json_data["countryCode_search"]
 
 # Set a user agent. Hey it's me!
-json_data[
-    "userAgent"
-] = "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0"
+json_data["userAgent"] = (
+    "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0"
+)
 
 # Open the output file. This is ghetto because we write each line individually
 # in each loop iteration below.
